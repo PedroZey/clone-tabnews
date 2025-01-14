26eb3045eef2eb41a5e19dbf36e4641f0520b2b2
Pedro <143122256+PedroZey@users.noreply.github.com>
2025-01-14 15:57:37 +0000

    apenas modificando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1>Eu te amo minha rosa e eu vou te amar por toda a eternidade!</h1>
+    return <><h1>Eu te amo minha rosa e eu vou te amar por toda a eternidade!</h1> 
+        <h1>Para a minha Evinha, com muito amor, Pedrão kkkk</h1></>
 }
 
 export default Home;
